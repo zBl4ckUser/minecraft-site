0462542b503cf3b4dd5c5723af26f1e2e1a830dc
zBl4ckUser <zBl4ckDev@protonmail.com>
2023-06-01 14:26:56 -0300

    update files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-
+function ExportToExcel(type, fn, dl) {
+    var elt = document.getElementById('tbl_exporttable_to_xls');
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl ?
+      XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
+      XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+ }
