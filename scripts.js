1becd79c15dd1ad631253a2a096aea59e763b36f
zBl4ckUser <zBl4ckDev@protonmail.com>
2023-06-02 19:54:32 -0300

    Atualização final

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-function ExportToExcel(type, fn, dl) {
-    var elt = document.getElementById('tbl_exporttable_to_xls');
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
-    return dl ?
-      XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
-      XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
- }
-
 function soundChange(){
   var audio = document.getElementById("background-music"); 
   if(audio.paused){
@@ -13,6 +5,31 @@ function soundChange(){
   }else{
     audio.pause();
   }
-  audio.volume = 0.105
+  audio.volume = 1
   
+}
+
+function loginMessage(){
+  const div_style = `
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    margin-left: 465px;
+    max-width: 400px;
+    border: 5px;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: green;
+    `
+  const div = document.createElement("div");
+  const div_msg = document.createTextNode("Você entrou com sucesso!");
+  div.classList.add("login-message")
+  div.appendChild(div_msg)
+
+  document.body.append(div)
+  
+  div.style.cssText = div_style
+
 }
